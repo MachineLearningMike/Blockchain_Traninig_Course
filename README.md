a8a96e31023e6b689f8f55fb216a8fddf8139565
MachineLearningMike <fleetpro@gmail.com>
2021-10-25 15:28:02 +0800

    debugging

--- README.md
+++ README.md
@@ -3,19 +3,20 @@ Courseware for Dapp development
 
 1. Clone this repository to your local machine
 
-2. 
+2. Setup node.js environment in the local folder:
 
 npm init --yes
 npm install --save-dev hardhat
 
-npx hardhat
-(Select Create an empty hardhat.config.js with your keyboard and hit enter.)
-
 npm install --save-dev @nomiclabs/hardhat-ethers ethers @nomiclabs/hardhat-waffle ethereum-waffle chai
 
-npm i dotenv
-npm i @nomiclabs/hardhat-etherscan
+npm i dotenv --save-dev
+npm i @nomiclabs/hardhat-etherscan --save-dev
 npm install hardhat-gas-reporter --save-dev
-npm i solidity-coverage
+npm i solidity-coverage --save-dev
 
 npm install --save-dev @nomiclabs/hardhat-web3 web3
+
+3. Install node_modules.
+
+npm install @openzeppelin/contracts --save-dev
--- hardhat.config.js
+++ hardhat.config.js
@@ -5,6 +5,7 @@ require("@nomiclabs/hardhat-waffle");
 require("hardhat-gas-reporter");
 require("solidity-coverage");
 
+
 // If you want to use Web3 an web3, instead of ethers:
 //require("@nomiclabs/hardhat-web3");
 // task action function receives the Hardhat Runtime Environment as second argument
@@ -13,6 +14,7 @@ require("solidity-coverage");
 //});
 //module.exports = {};
 // see for more: https://hardhat.org/plugins/nomiclabs-hardhat-web3.html
+
 
 // This is a sample Hardhat task. To learn how to create your own go to
 // https://hardhat.org/guides/create-task.html
@@ -31,19 +33,106 @@ task("accounts", "Prints the list of accounts", async (taskArgs, hre) => {
  * @type import('hardhat/config').HardhatUserConfig
  */
 module.exports = {
-  solidity: "0.8.4",
+  solidity: {
+    compilers: [
+      {
+        version: "0.8.4",
+      },
+      { // for uniswap v3
+        version: '0.7.6',
+        settings: {
+          optimizer: {
+            enabled: true,
+            runs: 0 //ORG 800
+          },
+          metadata: {
+            // do not include the metadata hash, since this is machine dependent
+            // and we want all generated code to be deterministic
+            // https://docs.soliditylang.org/en/v0.7.6/metadata.html
+            bytecodeHash: 'none',
+          },
+        },
+      },
+			{ // for Token_WETH
+				version: "0.4.18",
+				settings: {
+					optimizer: {
+						enabled: true,
+						runs: 200,
+					}
+				},
+			},
+    ],
+    overrides: {
+      // To save console.sol from being compiled in lower version.
+      "hardhat/console.sol": {
+        version: '0.8.0',
+        settings: {}  
+      },
+      // The following 7 overrides are used to compile OpenZeppelin_Fixed_Token.sol.
+      // You can't put version 0.8.0 in the regular compiler list, 
+      // because UniswapV3 code will accept that version of compiler, wrongfully.
+      "contracts/OpenZeppelin_Fixed_Token.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC721/IERC721Receiver.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC20/presets/ERC20PresetFixedSupply.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC20/extensions/ERC20Burnable.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC20/extensions/IERC20Metadata.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC20/IERC20.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/token/ERC20/ERC20.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+      "@openzeppelin/contracts/utils/Context.sol": {
+        version: '0.8.0',
+        settings: {}
+      },
+    }
+  },
   networks: {
-    ropsten: {
-      url: process.env.ROPSTEN_URL || "",
-      accounts:
-        process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
+    hardhat: {
+      allowUnlimitedContractSize: false,
     },
+    mainnet: {
+      url: `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    ropsten: {
+      url: `https://ropsten.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    rinkeby: {
+      url: `https://rinkeby.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    goerli: {
+      url: `https://goerli.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+    kovan: {
+      url: `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
+    },
+  },
+  etherscan: {
+    // Your API key for Etherscan
+    // Obtain one at https://etherscan.io/
+    apiKey: process.env.ETHERSCAN_API_KEY,
   },
   gasReporter: {
     enabled: process.env.REPORT_GAS !== undefined,
     currency: "USD",
-  },
-  etherscan: {
-    apiKey: process.env.ETHERSCAN_API_KEY,
   },
 };
